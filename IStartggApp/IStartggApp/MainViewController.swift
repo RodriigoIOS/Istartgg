f2efba098097eba152b14a8f3f36c6acd1f21040
Rodrigo <rodrigo@Julias-MacBook-Pro.local>
2024-11-13 14:19:42 -0300

    initial commit

---
+++ IStartggApp/IStartggApp/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IStartggApp
+//
+//  Created by Rodrigo on 13/11/24.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
